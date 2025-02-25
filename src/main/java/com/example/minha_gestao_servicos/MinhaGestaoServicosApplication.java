94715eef4e03cf9546e52f4e5fef07aab8098424
devlucascalado <devlucascalado@gmail.com>
2025-02-25 13:56:00 -0300

    commit

---
+++ src/main/java/com/example/minha_gestao_servicos/MinhaGestaoServicosApplication.java
@@ -0,0 +1,13 @@
+package com.example.minha_gestao_servicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaGestaoServicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaGestaoServicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/minha_gestao_servicos/MinhaGestaoServicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minha_gestao_servicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaGestaoServicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

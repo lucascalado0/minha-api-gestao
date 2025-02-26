0513d39b772e4e3e6708782145f39ec837608a55
devlucascalado <devlucascalado@gmail.com>
2025-02-26 18:55:11 -0300

    ajustando petcontroller

--- src/main/java/com/example/minha_gestao_servicos/controller/PetController.java
+++ src/main/java/com/example/minha_gestao_servicos/controller/PetController.java
@@ -4,7 +4,6 @@ package com.example.minha_gestao_servicos.controller;
 
 import com.example.minha_gestao_servicos.model.Cliente;
 import com.example.minha_gestao_servicos.model.Pet;
-import com.example.minha_gestao_servicos.repository.ClienteRepository;
 import com.example.minha_gestao_servicos.service.impl.ClienteServiceImpl;
 import com.example.minha_gestao_servicos.service.impl.PetServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,27 +26,27 @@ public class PetController {
     private ClienteServiceImpl clienteService;
 
 
-//    @PostMapping(value = "/save")
-//    public ResponseEntity<Pet> create(@RequestBody Pet pet){
-//
-//        pet = petService.create(pet);
-//
-//        return ResponseEntity.ok().body(pet);
-//    }
+    @PostMapping(value = "/save")
+    public ResponseEntity<Pet> create(@RequestBody Pet pet){
 
-    @PostMapping(name = "/save")
-    public ResponseEntity<Pet> adicionarPet(@RequestBody Pet pet) {
-        Optional<Cliente> clienteOptional = clienteService.findByCpf(pet.getTutor().getCpf());
-        if (clienteOptional.isPresent()) {
-            Cliente cliente = clienteOptional.get();
-            pet.setTutor(cliente);
-            cliente.getPets().add(pet);
-            Pet novoPet = petService.create(pet);
-            return ResponseEntity.status(HttpStatus.CREATED).body(novoPet);
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
+        pet = petService.create(pet);
+
+        return ResponseEntity.ok().body(pet);
     }
+
+//    @PostMapping(name = "/save")
+//    public ResponseEntity<Pet> adicionarPet(@RequestBody Pet pet) {
+//        Optional<Cliente> clienteOptional = clienteService.findByCpf(pet.getTutor().getCpf());
+//        if (clienteOptional.isPresent()) {
+//            Cliente cliente = clienteOptional.get();
+//            pet.setTutor(cliente);
+//            cliente.getPets().add(pet);
+//            Pet novoPet = petService.create(pet);
+//            return ResponseEntity.status(HttpStatus.CREATED).body(novoPet);
+//        } else {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+//        }
+//    }
 
 
 
